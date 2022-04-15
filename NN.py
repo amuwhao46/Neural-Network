b8d604bde807d42fd946697771c910bbc9332d29
Oke Amuwha <amuwhao46@gmail.com>
2022-04-14 22:39:59 -0400

    Almost all of the code was prewritten. Changed some parameters to see how the neural network reacts and learns

---
+++ NN.py
@@ -0,0 +1,122 @@
+from __future__ import absolute_import, division, print_function
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# MNSIT data parameters
+num_classes = 10 # Numbers being trained
+num_inputs = 784 # data features (img shape: 28*28).
+
+# Training parameters
+learning_rate = 0.001
+training_steps = 6000
+batch_size = 256
+display_step = 150
+
+# Neurons in hidden layer
+n_hidden_1 = 128 # 1st layer number of neurons.
+n_hidden_2 = 256 # 2nd layer number of neurons.
+
+# Import MNIST data
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path="mnist.npz")
+# Convert to float32.
+x_train, x_test = np.array(x_train, np.float32), np.array(x_test, np.float32)
+# Flatten images to 1-D vector of 784 features (28*28).
+x_train, x_test = x_train.reshape([-1, num_inputs]), x_test.reshape([-1, num_inputs])
+# Sets grayscale to black or white
+x_train, x_test = x_train / 255., x_test / 255.
+
+# Use tf.data API to shuffle and batch data.
+train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_data = train_data.repeat().shuffle(5000).batch(batch_size).prefetch(1)
+
+# Randomly generates neuron weights
+random_normal = tf.initializers.RandomNormal()
+
+weights = {
+    'h1': tf.Variable(random_normal([num_inputs, n_hidden_1])),
+    'h2': tf.Variable(random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(random_normal([n_hidden_2, num_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.zeros([n_hidden_1])),
+    'b2': tf.Variable(tf.zeros([n_hidden_2])),
+    'out': tf.Variable(tf.zeros([num_classes]))
+}
+
+# Model
+def neural_net(x):
+    # Hidden fully connected layer with 128 neurons.
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    # Apply sigmoid to layer_1 output for non-linearity.
+    layer_1 = tf.nn.sigmoid(layer_1)
+    
+    # Hidden fully connected layer with 256 neurons.
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    # Apply sigmoid to layer_2 output for non-linearity.
+    layer_2 = tf.nn.sigmoid(layer_2)
+    
+    # Output fully connected layer with a neuron for each class.
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    # Apply softmax to normalize the logits to a probability distribution.
+    return tf.nn.softmax(out_layer)
+
+    # Cross-Entropy loss function.
+def cross_entropy(y_pred, y_true):
+    # Encode label to a one hot vector.
+    y_true = tf.one_hot(y_true, depth=num_classes)
+    # Clip prediction values to avoid log(0) error.
+    y_pred = tf.clip_by_value(y_pred, 1e-9, 1.)
+    # Compute cross-entropy.
+    return tf.reduce_mean(-tf.reduce_sum(y_true * tf.math.log(y_pred)))
+
+# Accuracy metric.
+def accuracy(y_pred, y_true):
+    # Predicted class is the index of highest score in prediction vector (i.e. argmax).
+    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.cast(y_true, tf.int64))
+    return tf.reduce_mean(tf.cast(correct_prediction, tf.float32), axis=-1)
+
+# Stochastic gradient descent optimizer.
+optimizer = tf.optimizers.SGD(learning_rate)
+
+# Optimization process. 
+def run_optimization(x, y):
+    # Wrap computation inside a GradientTape for automatic differentiation.
+    with tf.GradientTape() as g:
+        pred = neural_net(x)
+        loss = cross_entropy(pred, y)
+        
+    # Variables to update, i.e. trainable variables.
+    trainable_variables = list(weights.values()) + list(biases.values())
+
+    # Compute gradients.
+    gradients = g.gradient(loss, trainable_variables)
+    
+    # Update W and b following gradients.
+    optimizer.apply_gradients(zip(gradients, trainable_variables))
+
+# Run training for the given number of steps.
+for step, (batch_x, batch_y) in enumerate(train_data.take(training_steps), 1):
+    # Run the optimization to update W and b values.
+    run_optimization(batch_x, batch_y)
+    
+    if step % display_step == 0:
+        pred = neural_net(batch_x)
+        loss = cross_entropy(pred, batch_y)
+        acc = accuracy(pred, batch_y)
+        print("step: %i, loss: %f, accuracy: %f" % (step, loss, acc))
+
+# Test model on validation set.
+pred = neural_net(x_test)
+print("Test Accuracy: %f" % accuracy(pred, y_test))
+
+# Predict 3 images from validation set.
+n_images = 3
+test_images = x_test[:n_images]
+predictions = neural_net(test_images)
+
+# Display image and model prediction.
+for i in range(n_images):
+    plt.imshow(np.reshape(test_images[i], [28, 28]), cmap='gray')
+    plt.show()
+    print("Model prediction: %i" % np.argmax(predictions.numpy()[i]))
